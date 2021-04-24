e9f24c54ef380e98ee35bb09d23fae0dd6b9c752
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-04-23 22:18:47 -0300

    Exercício Aula 13 - Laços - com desafios resolvidos

--- semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
+++ semana3/loops/js-vanilla-template/js-vanilla-template/src/index.js
@@ -52,3 +52,56 @@ for (let i = 0; i < arrayOriginal.length; i++){
 }
 let mensagem = `O maior número é ${arrayMaior} e o menor é ${arrayMenor}`;
     console.log(mensagem);
+
+
+//Desafio 1
+
+const numeroPensado = Number(prompt("Digite um número."));
+console.log("Que comecem os jogos!");
+let chuteNumero = Number(prompt("Chute um número."));
+let contador = 0;
+while(chuteNumero!==numeroPensado){
+    
+    if(chuteNumero>numeroPensado){
+        console.log("O número chutado foi: ",chuteNumero);
+        console.log("Errrrrrrrou, o número pensado é menor");           
+    } else if(chuteNumero<numeroPensado){
+        console.log("O número chutado foi: ",chuteNumero);
+        console.log("Errrrrrrrou, o número pensado é maior");
+    } 
+    chuteNumero = Number(prompt("Digite outro número."))
+    ++contador;
+}
+
+console.log("O número pensado foi:",numeroPensado);
+console.log("Acertou!!");
+console.log("O número tentativas foi:",contador);
+
+//Desafio 2
+
+
+
+let numeroPensado = Math.round((Math.random() * 100));
+console.log(numeroPensado)
+console.log("Que comecem os jogos!");
+let chuteNumero=Number(prompt("Chute um número."));
+let contador = 1;
+while(chuteNumero!==numeroPensado){
+    
+
+    if(chuteNumero>numeroPensado){
+        console.log("O número chutado foi: ",chuteNumero);
+        console.log("Errrrrrrrou, o número pensado é menor");           
+    } else if(chuteNumero<numeroPensado){
+        console.log("O número chutado foi: ",chuteNumero);
+        console.log("Errrrrrrrou, o número pensado é maior");
+    } 
+    chuteNumero=Number(prompt("Chute um número."));
+   contador++;
+}
+
+console.log("O número pensado foi:",numeroPensado);
+console.log("Acertou!!");
+console.log("O número tentativas foi:",contador);
+
+//Eu fiz o curso de lógica com js do Alura e tem um exercício desse. Para gerar o número aleatório é usado o Math.round((Math.random() * 100). O Math.round() arredonda e o Math.random() * 100 gera números aleatórios de 1 a 100
