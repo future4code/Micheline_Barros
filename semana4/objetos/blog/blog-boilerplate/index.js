da14e1c6adf67b38a61428e69ea7aa7f25ff66fc
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-04-29 13:42:49 -0300

    tentativa de manipular array

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -9,20 +9,25 @@ function post () {
     let inputConteudo = document.getElementById("conteudo-post");
 
     let conteudoDivBlog = document.getElementById("container-de-posts");
-    
+    console.log(arrayBlog[0])
     let objetoPost = {
         titulo: `${inputTitulo.value}`,
         autor: `${inputAutor.value}`,
         conteudo: `${inputConteudo.value}`
     }
     arrayBlog.push(objetoPost);
+    console.log(arrayBlog)
+    for(let post of arrayBlog){
+        conteudoDivBlog.innerHTML += `<h1>${arrayBlog[0]}</h1>`
+    }
     
-    conteudoDivBlog.innerHTML += `<h1>${inputTitulo.value}</h1>`
-    inputTitulo.value = "";
-    conteudoDivBlog.innerHTML += `<h3>${inputAutor.value}</h3>`
-    inputAutor.value = "";
-    conteudoDivBlog.innerHTML += `<p>${inputConteudo.value}</p>`
-    inputConteudo.value = "";
-    console.log(arrayBlog);
+    
+    // conteudoDivBlog.innerHTML += `<h1>${inputTitulo.value}</h1>`
+    // inputTitulo.value = "";
+    // conteudoDivBlog.innerHTML += `<h3>${inputAutor.value}</h3>`
+    // inputAutor.value = "";
+    // conteudoDivBlog.innerHTML += `<p>${inputConteudo.value}</p>`
+    // inputConteudo.value = "";
+    // console.log(arrayBlog);
 }
 
