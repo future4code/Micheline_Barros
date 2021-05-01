df2047e3beda24b2b6fb138975fba929889b8de0
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-05-01 00:26:02 -0300

    8º exercício - Lista-de-exercícios

--- semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
+++ semana4/lista-de-exercicios/lista-js-boilerplate/exercicios.js
@@ -95,7 +95,16 @@ function retornaNNumerosPares(n) {
 // Exercício 8
 
 function checaTriangulo(a, b, c) {
+  
   // implemente sua lógica aqui
+  if(a===b && b===c){
+   return "Equilátero"
+  } else if (a !== b && b !== c) {
+   return "Escaleno";
+  } else {
+   return "isósceles";
+  }
+ 
 }
 
 // Exercício 9
