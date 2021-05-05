101c59e0c26a843eb3d5f07507a57fd5f78ca4e5
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-05-05 16:46:11 -0300

    Aula 20 - Componentes de Classe + Estados - Exercício insta4

---
+++ semana5/insta4/insta4/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+import micheline from './img/micheline.jpg'
+import fotoMicheline from './img/baciaCactos.jpg'
+import pipoca from './img/pipoca.jpg'
+import fotoPipoca from './img/pipocaGostoso.jpg'
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+        <Post
+          nomeUsuario={'Micheline Barros'}
+          fotoUsuario={micheline}
+          fotoPost={fotoMicheline}
+        />
+        <Post
+          nomeUsuario={'Pipoca Barros'}
+          fotoUsuario={pipoca}
+          fotoPost={fotoPipoca}
+        />
+      </MainContainer>
+    );
+  }
+}
+
+export default App;
