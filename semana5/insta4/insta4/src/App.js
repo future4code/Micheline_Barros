81520086dc19094933732b7a87374bc6b29068f0
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-05-10 14:12:24 -0300

    Correção

--- semana5/insta4/insta4/src/App.js
+++ semana5/insta4/insta4/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styled from 'styled-components'
 import Post from './components/Post/Post';
-import micheline from './img/micheline.jpg'
-import fotoMicheline from './img/baciaCactos.jpg'
-import pipoca from './img/pipoca.jpg'
-import fotoPipoca from './img/pipocaGostoso.jpg'
+// import micheline from './img/micheline.jpg'
+// import fotoMicheline from './img/baciaCactos.jpg'
+// import pipoca from './img/pipoca.jpg'
+// import fotoPipoca from './img/pipocaGostoso.jpg'
 
 const MainContainer = styled.div`
   display: flex;
@@ -17,28 +17,45 @@ const MainContainer = styled.div`
 class App extends React.Component {
 
   state = {
-    postagem: []
+    postagens: [
+      {
+        nomeUsuario: "Paulinha",
+        fotoUsuário: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: "Micheline Barros",
+        fotoUsuario: "./img/micheline.jpg",
+        fotoPost: "./img/baciaCactos.jpg"
+      },
+      {
+        nomeUsuario: "Pipoca Barros",
+        fotoUsuário: "./img/pipoca.jpg",
+        fotoPost: "./img/pipocaGostoso.jpg"
+      },
+      {
+        nomeUsuario: "",
+        fotoUsuário: "",
+        fotoPost: ""
+      }
+    ]
   }
 
 
   render() {
+
+    const linhaDoTempo = this.state.postagens.map((postar) => {
+      return(
+        <Post
+        nomeUsuario = {postar.nomeUsuario}
+        fotoUsuário = {postar.fotoUsuario}
+        fotoPost = {postar.fotoPost} 
+        />
+      );
+    });
     return (
       <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Micheline Barros'}
-          fotoUsuario={micheline}
-          fotoPost={fotoMicheline}
-        />
-        <Post
-          nomeUsuario={'Pipoca Barros'}
-          fotoUsuario={pipoca}
-          fotoPost={fotoPipoca}
-        />
+        {linhaDoTempo}
       </MainContainer>
     );
   }
