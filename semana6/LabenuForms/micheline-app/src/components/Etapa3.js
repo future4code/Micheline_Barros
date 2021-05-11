35fc771f45a9b000b41790392fb1f8c8b400635d
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-05-10 21:30:25 -0300

    Corrigi input da etapa 3

--- semana6/LabenuForms/micheline-app/src/components/Etapa3.js
+++ semana6/LabenuForms/micheline-app/src/components/Etapa3.js
@@ -20,8 +20,6 @@ export default class Etapa3 extends React.Component {
                     <p>5. Por que você não terminou um curso de graduação?</p>
                     <input />
                     <p>6. Você fez algum curso complementar?</p>
-                    <Imput6 />
-                    <br />
                     <select>
                         <option value="Nenhum">Nenhum</option>  
                         <option value="Curso técnico">Curso técnico</option>  
