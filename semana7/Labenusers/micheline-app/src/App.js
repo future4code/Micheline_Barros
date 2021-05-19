4f32c9354dcf8d8141fd74762a4f1aae2b081c10
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-05-19 16:39:40 -0300

    Aula 27 - async & await - Corrigindo exercício Labenusers

--- semana7/Labenusers/micheline-app/src/App.js
+++ semana7/Labenusers/micheline-app/src/App.js
@@ -1,122 +1,54 @@
 import React from 'react';
 import './index.css';
 import axios from 'axios';
+import TelaCadastro from './components/TelaCadastro';
+import TelaLista from './components/TelaLista';
+
 
 const URL_BASE = 'https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users'
 
 export default class App extends React.Component {
   state ={
-   listaUsuarios:[],
-   inputName:'',
-   inputEmail:''
-  }
-  componentDidMount(){
-    this.getAllUsers();
+    telaAtual:'cadastro' //premitirá a rederização de tela
   }
 
-handleName = (event) => {
-  this.setState({inputName: event.target.value})
-  console.log("handleName", event.target.value)
-}
-// teclaEnter = (event) => {
-//   if (event.keyCode === 13){ //essa função serve pra habilitar o uso do enter pra ativar o botão criar usuário, onde o key.code 13é o código que identifica a tecla enter
-//     this.criarUsuario();
-//   }
-// }
-
-handleEmail = (event) => {
-  this.setState({inputEmail: event.target.value})
-  console.log("handleName", event.target.value)
-}
-
-criarUsuario = () => {
-  const header ={
-    headers:{
-      Authorization: "micheline-barros-paiva"
+  alternaTela = () =>{
+    switch (this.state.telaAtual){
+      case 'cadastro': 
+        return <TelaCadastro lista={this.irParaLista}/>
+      case 'lista':
+        return <TelaLista cadastro={this.irParaCadastro}/>
+      default:
+        return <p>Erro! Página não encontrada</p>
     }
-  };
-
-  const body = {
-    name: this.state.inputName,
-    email: this.state.inputEmail
   }
 
-  axios.post(URL_BASE, body, header).then(() =>{ //o body sempre tem que ser o segundo elemento a ser inserido
-    alert('Usuário adicionado com sucesso');
-    // this.setState({inputName:''}); //limpar o campo de input
-    // this.setState({inputEmail:''}) //limpar o campo de input
-    this.getAllUsers(); //chamo minha função no final pra atualizar meu estado
-  })
-  .catch((err) => {
-   alert(err.response.data);
-  });
-};
+  irParaCadastro = () => {
+    this.setState({telaAtual:'cadastro'})
+  }
 
-getAllUsers = () => { //mostra informações dos usuários como nome e email
-  const header = {
-    headers:{
-      Authorization: "micheline-barros-paiva"
-    }
-  };
+  irParaLista = () => {
+    this.setState({telaAtual: 'lista'})
+  }
 
-  axios.get(URL_BASE, header)
-  .then((res) => {
-    this.setState({listaUsuarios: res.data});
-    console.log(res)
-  })
-  .catch((err) => {
-    alert(err.response.data);
-    console.log(err)
-  });
-};
 
-deleteUser = (usuarioId) => { 
-  const header = {
-    headers:{
-      Authorization: "micheline-barros-paiva"
-    }
-  };
+  
 
-  axios.delete(`${URL_BASE}`+usuarioId, header)
-  .then((res) => {
-    this.setState({listaUsuarios: res.data});
-   
-  })
-  .catch((err) => {
-    alert(err.response.data);
-    console.log(err)
-    
-  });
-};
+
+
+
+
+
 
 
   render(){
-    const listaDeUsuarios = this.state.listaUsuarios.map((usuario) => {
-      return(
-        <li key = {usuario.id}>{usuario.name}<p></p><button onClick={()=>this.deleteUser(this.usuarioId)}>remover</button></li>
-        
-      )
-    });
-    console.log('entrou na lista')
-    console.log(listaDeUsuarios)
     
     return (
       <div className='corpo'>
-        <h2>Labenusers</h2>
-        <button>Trocar de página</button>
+       
+        {this.alternaTela()}
         <hr />
-        <h3>Criar usuário</h3>
-        <input placeholder='nome' onChange={this.handleName} value={this.state.inputName} />
-        <input placeholder='E-mail' onChange={this.handleEmail} value={this.state.inputEmail} />
-        <button onClick={this.criarUsuario} >criar</button>
-        <div>
-        <h2>Labenusers</h2>
-        <button>Trocar de página</button>
-        <hr />
-        <h3>Lista de usuários</h3>
-          {listaDeUsuarios}
-        </div>
-
+    
       </div>
     );
   }
