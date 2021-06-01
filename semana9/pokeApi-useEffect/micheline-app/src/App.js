68c5628fc584d7ecd8edf9a1104d7ed4aa7ef2f9
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-06-01 16:47:11 -0300

    Exercício Aula 33 - Hook useEffect()

---
+++ semana9/pokeApi-useEffect/micheline-app/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import styled from 'styled-components'
+
+
+function App() {
+  const [pokeList, setPokeList] = useState([])
+  const [pokeName, setPokeName] = useState('')
+
+  const 
+
+
+  return (
+    <div>
+     
+    </div>
+  );
+}
+
+export default App;
