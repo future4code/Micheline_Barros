c218ef6dfb527fa511e5f965ecd1592b32d94f74
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-06-01 18:24:02 -0300

    criando o código do app

--- semana9/pokeApi-useEffect/micheline-app/src/App.js
+++ semana9/pokeApi-useEffect/micheline-app/src/App.js
@@ -1,18 +1,42 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import styled from 'styled-components'
+import styled, { ThemeConsumer } from 'styled-components'
 
 
 function App() {
   const [pokeList, setPokeList] = useState([])
   const [pokeName, setPokeName] = useState('')
 
-  const 
+  useEffect(() =>{
+    duzentosPokemon()
+  },[])
 
+  const duzentosPokemon = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/?limit=200')
+    .then((res) => {
+      setPokeList(res.data.results)
+      console.log(res.data.results)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const onChangeSelectPokeName = (event) => {
+    setPokeName(event.target.value)
+  }
 
   return (
+    
     <div>
-     
+      <select onChange={onChangeSelectPokeName}>
+        <option value=''></option>
+        {pokeList.map((namePoke) => {
+          console.log(namePoke.name)
+          return(
+            <option value={namePoke.name}>{namePoke.name}</option>
+          )
+        })}
+      </select>
     </div>
   );
 }
