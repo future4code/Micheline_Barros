e9ee51d2b274f899b37aa5c26fc1c98b49a6368e
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-07-13 00:19:33 -0300

    resposta exercício 1 c

--- semana15/node-npm/respostas.js
+++ semana15/node-npm/respostas.js
@@ -6,3 +6,10 @@ const idade = process.argv[3]
 
 console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
 
+//c)
+const nome = process.argv[2]
+const idade = process.argv[3]
+
+const idadeMaisSete = Number(process.argv[3]) + 7
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Daqui 7 anos você terá ${idadeMaisSete}.`)
