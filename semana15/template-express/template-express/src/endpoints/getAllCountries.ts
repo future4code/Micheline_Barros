076abb8e446a53efa3eed983f38a4151b47a85f9
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-07-15 00:30:42 -0300

    Endpoint 1

--- semana15/template-express/template-express/src/endpoints/getAllCountries.ts
+++ semana15/template-express/template-express/src/endpoints/getAllCountries.ts
@@ -1,9 +1,19 @@
+import { countries } from './../data';
 import { Request, Response } from "express";
+
+const paises = countries.map((pais) => {
+    return(
+        {
+            "id": pais.id,
+            "name": pais.name
+        }  
+    )
+})
 
 const getAllCountries = ( //recebe dois parâmetros. Sempre o request/req virá antes do response/res
     request: Request,              //tipagem que dá acesso aos métodos
     response: Response
 ) => {
-    response.send('Hello from express')
+    response.send(paises)
 }
 export default getAllCountries;
