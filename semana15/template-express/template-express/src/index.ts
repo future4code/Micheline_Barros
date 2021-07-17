3bd9715aa6e10fd07a044a7c5a47c5b2bb3283e8
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-07-17 17:50:01 -0300

    Endpoint 3

--- semana15/template-express/template-express/src/index.ts
+++ semana15/template-express/template-express/src/index.ts
@@ -1,10 +1,14 @@
 import app from './app';
 import getAllCountries from './endpoints/getAllCountries';
 import getAllCountryById from './endpoints/getAllCountryById';
+import getSearch from './endpoints/getSearch';
 
 app.get("/countries", getAllCountries) 
 
 //app.method(path, handler). A função getAllCountries será acionada quando chegar uma requisição para o caminho /countries
 
+app.get("/countries/search", getSearch)
 
 app.get("/countries/:id", getAllCountryById)
+
+
