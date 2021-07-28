e651fe3a5217630b4337f6892a80d53f3bd8d589
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-07-28 12:58:20 -0300

    corrigindo arquivos do exercício aula 49

--- semana17/Aula49-filtros-ordenacao-paginacao/template-filtros-ordenacao-paginacao/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { app } from "./app";
-import { getAllRecipes } from "./endpoints/getAllRecipes";
-import { getRecipesNome } from "./endpoints/getRecipesNome";
-
-
-app.get("/recipes", getRecipesNome)
