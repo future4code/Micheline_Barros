d438c8abefccf4c314d0244825c008e1fbe93b2d
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-07-28 14:07:25 -0300

    Exercício 1b

--- semana17/Aula49-filtros-ordenacao-paginacao/Exercicio-filtros-ordenacao-paginacao/src/index.ts
+++ semana17/Aula49-filtros-ordenacao-paginacao/Exercicio-filtros-ordenacao-paginacao/src/index.ts
@@ -1,8 +1,13 @@
 import { getAllUsersName } from './endpoints/getAllUsersName';
 import { app } from "./app";
+import { getAllUsersType } from './endpoints/getAllUsersType';
 // import { getAllUsers } from './endpoints/getAllUsers';
 
 
 // app.get("/users", getAllUsers)
 
 app.get("/users", getAllUsersName)
+
+
+app.get("/users/:type", getAllUsersType)
+
