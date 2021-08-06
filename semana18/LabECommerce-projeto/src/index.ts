25104721292d4dce56bcf00aca756c97d18836fe
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-08-06 16:55:13 -0300

    Endpoint 1 - tentando pelo menos

--- semana18/LabECommerce-projeto/src/index.ts
+++ semana18/LabECommerce-projeto/src/index.ts
@@ -1,4 +1,7 @@
-
+import { Request, Response } from 'express';
+import { app } from './app';
+import {connection} from './data/connection';
+import moment from 'moment'
 
 
 export class User{
@@ -44,3 +47,52 @@ export class Purchase{
         total: number
     ){}
 }
+
+
+//Endpoint 1
+class Usuarios extends User{
+    createUser = async(
+        idUser:string,
+        name: string,
+        email: string,
+        age: number
+        ): Promise<void> => {
+        await connection.raw(`
+            insert into user(idUser, name, email, age)
+            values(
+                ${idUser},
+                "${name}",
+                "${email}",
+                "${age}"
+            )
+        `)
+        }
+}
+
+
+app.post("/user", async(req: Request, res: Response): Promise<void> => {
+    try{
+        if(
+            !req.body.name ||
+            !req.body.email ||
+            !req.body.age
+        ){
+            throw new Error("Verifique se todos os campos foram preenchidos corretamente!")
+        }
+
+
+        const idUser: string = Date.now() + Math.random().toString()
+        new Usuarios(
+            idUser,
+            req.body.name,
+            req.body.email,
+            req.body.age,
+        )
+        res.status(200).send({mensage:"Usuário criado com sucesso!", id: idUser})
+
+    } catch(error){
+        res.status(400).send(error.sqlMessage || error.message)
+    }
+})
+
+
