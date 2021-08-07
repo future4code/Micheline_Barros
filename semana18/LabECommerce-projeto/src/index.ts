89f72230c260dba87e060d93fb1c4e9f097e6b71
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-08-07 17:44:43 -0300

    Endpoint 1

--- semana18/LabECommerce-projeto/src/index.ts
+++ semana18/LabECommerce-projeto/src/index.ts
@@ -1,18 +1,28 @@
 import { Request, Response } from 'express';
 import { app } from './app';
-import {connection} from './data/connection';
+import {BaseDataBase } from './data/BaseDataBase';
 import moment from 'moment'
 
 
+
 export class User{
-    
+    idUser:string;
+    name: string;
+    email: string;
+    age: number;
     constructor(
         idUser:string,
         name: string,
         email: string,
         age: number
     ){
-
+        this.idUser = idUser,
+        this.name = name,
+        this.email = email,
+        this.age = age
+    }
+    getIdUser(): string{
+        return this.idUser
     }
 }
 
@@ -50,25 +60,18 @@ export class Purchase{
 
 
 //Endpoint 1
-class Usuarios extends User{
-    createUser = async(
-        idUser:string,
-        name: string,
-        email: string,
-        age: number
-        ): Promise<void> => {
-        await connection.raw(`
-            insert into user(idUser, name, email, age)
-            values(
-                ${idUser},
-                "${name}",
-                "${email}",
-                "${age}"
-            )
-        `)
+export class UserDataBase extends BaseDataBase {
+    public createUser = async(user: User) => {
+        await BaseDataBase.connection("user")
+            .insert({
+                idUser: user.getIdUser(),
+                name: user.name,
+                email: user.email,
+                age: user.age
+            })
         }
-}
 
+}
 
 app.post("/user", async(req: Request, res: Response): Promise<void> => {
     try{
@@ -82,17 +85,19 @@ app.post("/user", async(req: Request, res: Response): Promise<void> => {
 
 
         const idUser: string = Date.now() + Math.random().toString()
-        new Usuarios(
+        const newUser = new User(
             idUser,
             req.body.name,
             req.body.email,
             req.body.age,
         )
+        new UserDataBase().createUser(newUser)
+
         res.status(200).send({mensage:"Usuário criado com sucesso!", id: idUser})
 
     } catch(error){
         res.status(400).send(error.sqlMessage || error.message)
     }
-})
+});
 
 
