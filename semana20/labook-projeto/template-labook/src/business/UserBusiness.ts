c051abfa395c81a9ebd735f2a20b125f33442176
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-08-20 17:53:01 -0300

    Validação do signup

--- semana20/labook-projeto/template-labook/src/business/UserBusiness.ts
+++ semana20/labook-projeto/template-labook/src/business/UserBusiness.ts
@@ -8,6 +8,23 @@ export class UserBusiness{
 
     public  signup = async ({name, email, password}: any) => {
 
+        
+        if(name.length < 5 || !name){
+            console.log ("letra name",name.lenght)
+            throw new Error ("O campo nome não poderá estar em branco e deverá ter, no mínimo, 5 caracteres.");
+            
+        }
+
+
+        if(!email || email.indexOf("@") === -1){
+            throw new Error ("Email inválido.")
+        }
+
+        if(!password || password.length < 6){
+            throw new Error ("senha inválida. Informe uma senha com, no mínimo, 6 caracteres.")
+        }
+
+        
         const ig = new IdGenerator();
         const id = ig.generateId();
 
