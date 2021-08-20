d4585362035d938b9a6244453bd4cadb0be2334f
https://github.com/Michelineffb <michelinemm@gmail.com>
2021-08-20 00:44:07 -0300

    criando primeiros arquivos

---
+++ semana20/labook-projeto/src/services/Authenticator.ts
@@ -0,0 +1,26 @@
+import * as jwt from "jsonwebtoken"
+import { authenticationData } from "../types/user"
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const generateToken = (
+   payload: authenticationData
+): string => {
+   return jwt.sign(
+      payload,
+      process.env.JWT_KEY as string,
+      {
+         expiresIn: "24min"
+      }
+   )
+}
+
+export const getTokenData = (
+   token: string
+): authenticationData => {
+   return jwt.verify(
+      token,
+      process.env.JWT_KEY as string
+   ) as authenticationData
+}
